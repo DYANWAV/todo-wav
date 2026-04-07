e4f1adf5531eb8e12d3f9155f85ecf0001d5fc25
dyanwav <diego6mm@gmail.com>
2026-04-07 03:12:50 -0400

    feat(tasks): improve TaskItem with toggle and expansion

--- src/components/task-item.tsx
+++ src/components/task-item.tsx
@@ -1,8 +1,16 @@
 import { formatDate } from '@/lib/format-date'
 import { cn } from '@/lib/utils'
 import { useTaskStore, type Task } from '@/store/task-store'
-import { Calendar, EllipsisVertical, Eye, Pencil, Trash } from 'lucide-react'
+import {
+  Calendar,
+  ChevronDown,
+  EllipsisVertical,
+  Eye,
+  Pencil,
+  Trash,
+} from 'lucide-react'
 
+import { useState } from 'react'
 import { Link } from 'react-router'
 import { Badge } from './ui/badge'
 import { Button } from './ui/button'
@@ -27,23 +35,33 @@ interface Props {
 }
 
 export const TaskItem = ({ task }: Props) => {
+  const [open, setOpen] = useState(false)
   const deleteTask = useTaskStore(x => x.deleteTask)
+  const toggleCompleted = useTaskStore(x => x.toggleCompleted)
+
+  const onTaskOpen = () => {
+    setOpen(!open)
+  }
 
   return (
     <Item className={cn('flex')} variant={'outline'}>
       <ItemMedia>
-        <Checkbox className={cn('size-5', 'rounded-full', '')} />
+        <Checkbox
+          checked={task.completed}
+          onCheckedChange={() => toggleCompleted(task.id)}
+          className={cn('size-5', 'rounded-full', '')}
+        />
       </ItemMedia>
 
       <ItemContent className={cn('gap-1 flex-1 min-w-0')}>
         <Link to={`/task/${task.id}`} unstable_mask={`/task/${task.id}`}>
-          <ItemTitle className={cn('truncate text-base')}>
+          <ItemTitle className={cn('text-base', open && 'line-clamp-3')}>
             {task.title}
           </ItemTitle>
         </Link>
 
         {task.description && (
-          <ItemDescription className={cn('truncate')}>
+          <ItemDescription className={cn(!open && 'truncate')}>
             {task.description}
           </ItemDescription>
         )}
@@ -51,19 +69,23 @@ export const TaskItem = ({ task }: Props) => {
         {task.dueDate && (
           <Badge variant={'secondary'}>
             <Calendar />
-            {formatDate(task.dueDate)}
+            {formatDate(task.dueDate, open ? 'long' : 'short')}
           </Badge>
         )}
       </ItemContent>
 
+      <Button
+        variant={'ghost'}
+        size={'icon'}
+        onClick={() => onTaskOpen()}
+        className={cn('self-start rounded-full')}
+      >
+        <ChevronDown className={cn(open && 'rotate-180', 'transition')} />
+      </Button>
+
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <ItemActions
-            className={cn(
-              'self-start',
-              task.dueDate === undefined && 'self-center',
-            )}
-          >
+          <ItemActions className={cn('self-start')}>
             <Button variant="ghost" size="icon">
               <EllipsisVertical />
             </Button>
